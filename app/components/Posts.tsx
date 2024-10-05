b6c5835d9332c211776d7dcab7c846f62bfc17c4
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-10-05 10:06:09 +0800

    Move posts section date string under post title

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function BlogPosts(props: Props) {
   const posts = props.posts ?? getBlogPosts();
   return (
-    <div>
+    <ol>
       {posts
         .sort((a, b) => {
           if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
@@ -18,21 +18,22 @@ export default function BlogPosts(props: Props) {
           return 1;
         })
         .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/${postSubPath}/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
+          <li key={post.slug}>
+            <Link
+              className="flex flex-col space-y-1 mb-4"
+              href={`/${postSubPath}/${post.slug}`}
+            >
+              <div className="w-full flex flex-col space-x-0 md:space-x-2">
+                <h3 className="text-xl text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.metadata.title}
+                </h3>
+                <p className="text-xs text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums whitespace-nowrap">
+                  {formatDate(post.metadata.publishedAt, false)}
+                </p>
+              </div>
+            </Link>
+          </li>
         ))}
-    </div>
+    </ol>
   );
 }
