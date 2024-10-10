33d4353931fceda05c41412c9463d6009c4d55f9
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-10-10 22:42:45 +0800

    Extract /posts CSR page component to <PostsPage />

---
+++ app/posts/PostsPage.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { notFound, useSearchParams } from 'next/navigation';
+import BlogPosts from 'app/components/Posts';
+import { type Post } from 'app/posts/utils';
+import Paginator, { PAGE_SIZE, START_PAGE } from 'app/components/Paginator';
+
+type Props = {
+  posts: Post[];
+};
+
+export default function CsrPage({ posts }: Props) {
+  const pageQuery = useSearchParams().get('page');
+  const pageCount = Math.ceil(posts.length / PAGE_SIZE);
+  if (!!pageQuery && !isValidPageQuery(pageQuery, pageCount)) {
+    notFound();
+  }
+
+  const currentPage = isNaN(Number(pageQuery)) ? START_PAGE : Number(pageQuery);
+  const postsByPage = posts.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">All Posts</h1>
+      <BlogPosts posts={postsByPage} />
+      <Paginator count={pageCount} page={currentPage} />
+    </section>
+  );
+}
+
+function isValidPageQuery(value: string, pageCount: number): boolean {
+  const valueNumber = Number(value);
+  return !isNaN(valueNumber) && valueNumber >= START_PAGE && valueNumber <= pageCount;
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,8 @@
+import { Suspense } from 'react';
 import { type Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import BlogPosts from 'app/components/Posts';
 import { postSubPath } from 'app/constants';
 import { getBlogPosts } from 'app/posts/utils';
-import Paginator, { PAGE_SIZE, START_PAGE } from 'app/components/Paginator';
+import CsrPage from './PostsPage';
 
 export const metadata: Metadata = {
   title: 'Blog',
@@ -13,35 +12,11 @@ export const metadata: Metadata = {
   },
 };
 
-type Props = {
-  searchParams: {
-    page?: string;
-  };
-};
-
-export default function Page(props: Props) {
-  const {
-    searchParams: { page: pageQuery },
-  } = props;
-
+export default function Page() {
   const posts = getBlogPosts();
-  const pageCount = Math.ceil(posts.length / PAGE_SIZE);
-  if (!!pageQuery && !isValidPageQuery(pageQuery, pageCount)) {
-    notFound();
-  }
-
-  const currentPage = isNaN(Number(pageQuery)) ? START_PAGE : Number(pageQuery);
-  const postsByPage = posts.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
   return (
-    <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">All Posts</h1>
-      <BlogPosts posts={postsByPage} />
-      <Paginator count={pageCount} page={currentPage} />
-    </section>
+    <Suspense>
+      <CsrPage posts={posts} />
+    </Suspense>
   );
-}
-
-function isValidPageQuery(value: string, pageCount: number): boolean {
-  const valueNumber = Number(value);
-  return !isNaN(valueNumber) && valueNumber >= START_PAGE && valueNumber <= pageCount;
 }
