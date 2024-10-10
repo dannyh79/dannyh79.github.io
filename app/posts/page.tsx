478c2ac0a2b7fd8de0fdfca2c2a83e0764409fee
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-10-10 20:57:24 +0800

    Implement pagination in app/posts/page.tsx

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -33,26 +33,34 @@ export default function Page(props: Props) {
   const {
     searchParams: { page: pageQuery },
   } = props;
+
   const posts = getBlogPosts();
   const currentPage = isNaN(Number(pageQuery)) ? startPage : Number(pageQuery);
-  const pageItems = posts.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+  const postsByPage = posts.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+  const pageCount = Math.ceil(posts.length / PAGE_SIZE);
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">All Posts</h1>
-      <BlogPosts posts={pageItems} />
+      <BlogPosts posts={postsByPage} />
       <Pagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious href={{ query: { page: currentPage - 1 } }} />
+            <PaginationPrevious
+              href={{ query: { page: currentPage === startPage ? startPage : currentPage - 1 } }}
+            />
           </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
+          {Array.from({ length: pageCount }, (_, i) => i + startPage).map((page) => (
+            <PaginationItem key={page}>
+              <PaginationLink href={{ query: { page } }}>{page}</PaginationLink>
+            </PaginationItem>
+          ))}
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
           <PaginationItem>
-            <PaginationNext href={{ query: { page: currentPage + 1 } }} />
+            <PaginationNext
+              href={{ query: { page: currentPage === pageCount ? currentPage : currentPage + 1 } }}
+            />
           </PaginationItem>
         </PaginationContent>
       </Pagination>
