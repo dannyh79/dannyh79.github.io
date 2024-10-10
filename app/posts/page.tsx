24dce1cb00c65864e55d09d8b24bd27438bbb647
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-10-10 21:13:20 +0800

    Extract Paginator from app/posts/page.tsx <Page />

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -41,28 +41,40 @@ export default function Page(props: Props) {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">All Posts</h1>
       <BlogPosts posts={postsByPage} />
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious
-              href={{ query: { page: currentPage === START_PAGE ? START_PAGE : currentPage - 1 } }}
-            />
-          </PaginationItem>
-          {Array.from({ length: pageCount }, (_, i) => i + START_PAGE).map((page) => (
-            <PaginationItem key={page}>
-              <PaginationLink href={{ query: { page } }}>{page}</PaginationLink>
-            </PaginationItem>
-          ))}
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext
-              href={{ query: { page: currentPage === pageCount ? currentPage : currentPage + 1 } }}
-            />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <Paginator count={pageCount} page={currentPage} />
     </section>
+  );
+}
+
+type PaginatorProps = {
+  count: number;
+  page: number;
+};
+
+function Paginator(props: PaginatorProps) {
+  const { count, page: currentPage } = props;
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            href={{ query: { page: currentPage === START_PAGE ? START_PAGE : currentPage - 1 } }}
+          />
+        </PaginationItem>
+        {Array.from({ length: count }, (_, i) => i + START_PAGE).map((page) => (
+          <PaginationItem key={page}>
+            <PaginationLink href={{ query: { page } }}>{page}</PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext
+            href={{ query: { page: currentPage === count ? currentPage : currentPage + 1 } }}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
   );
 }
