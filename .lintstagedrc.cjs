e43ff6a0c9747ead883b12a8251be86c50d3896a
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2026-01-02 21:10:37 +0800

    Include type check in lint-staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,7 @@
 // Ref: https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
 
 module.exports = {
+  '*.{ts,tsx}': 'tsc --noEmit --project tsconfig.test.json',
   '*.{js,cjs,jsx,ts,tsx}': 'eslint',
   '*.{js,cjs,jsx,ts,tsx}': 'vitest --run --passWithNoTests',
   '*.{cjs,css,js,json,jsx,md,mdx,ts,tsx}': 'prettier -w',
